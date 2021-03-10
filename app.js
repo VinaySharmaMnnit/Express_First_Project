0e6d0c1b887284f937a1878c1d49c9e8027e4401
VinaySharmaMnnit <vinayshrotriya2021999@gmail.com>
2021-03-10 21:17:30 +0530

    Intial Commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const express=require('express');
+const hbs = require('hbs');
+
+var app=express();
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    console.log(now);
+    next();
+})
+const users=[
+    {'name':'Vinay','age':21},
+    {'name':'Om','age':22}
+]
+
+
+
+const posts=[
+    {title:'Bank',
+     description:'Here people can put there money'
+    },
+    {title:'Sea',
+     desription:'75% of World oxygen comes from sea plants'   
+}
+]
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        PageTitle:'Home Page',
+        siteName:'Tutorial',
+        name:'Vinay'
+    })
+});
+
+app.get('/users',(req,res)=>{
+    res.send(users);
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        PageTitle: 'About Page',
+        currentYear:new Date().getFullYear(),
+        name:'Vinay'
+    });
+})
+
+app.get('/users/:name',(req,res)=>{
+    console.log(req.params);
+    const {name}= req.params;
+    const user=users.find((user)=>user.name===name);
+    if(user)res.status(200).send(user);
+    else res.status(404).send("Not found");
+})
+
+app.get('/posts/:title',(req,res)=>{
+    console.log(req.query);
+}
+);
+
+app.get('/posts',(req,res)=>{
+    res.send(posts);
+})
+
+app.listen('3000',()=>{
+    console.log("Server is running");
+})
